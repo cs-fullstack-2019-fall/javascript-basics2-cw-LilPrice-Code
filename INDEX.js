db2e7bd88cdf8460609065946002f16694e1c7f5
LilPrice-Code <littleprice002@gmail.com>
2019-08-07 11:38:21 -0500

    Completed all 4 Problems

---
+++ INDEX.js
@@ -0,0 +1,31 @@
+//Question 1
+//Create two variables. One should equal “My name is: “ and the other should equal your name. Print the two variables in one print message.
+
+// var startString = "My name is: ";
+// var devName = "Shoshard";
+// alert(startString + devName);
+
+//Question 2
+// Ask the user “What is your balance”. Make sure to save that answer in a variable balance. Subtract 10 from that balance. Print "You lost $10. Your new balance is: [THE BALANCE]".
+
+// var userBal = parseInt(prompt("What is your balance?"));
+// var newBal = userBal - 10;
+// alert("You just lost $10!");
+// alert("Your new balance is: $" + newBal);
+
+
+//Question 3
+//Create a variable. Ask the user to “Enter your number grade”. Ask the user “Enter your extra credit". Print the grade plus extra credit.
+
+// var uGrade = parseInt(prompt("Enter your grade."));
+// var uExtra = parseInt(prompt("Enter your extra credit."));
+// var finalGrade = uGrade + uExtra;
+// alert("Your final grade is: " + finalGrade);
+
+
+//Question 4
+//Create a variable called balance and save it after asking the user “What is your balance?”. Create a variable called deduction and save it when asking the user “How much do you want to deduct?” Print the remaining balance after subtracting the deduction.
+
+// var balance = parseInt(prompt("What is your balance?"));
+// var deduction = parseInt(prompt("How much do you want to deduct?"));
+// alert("Your remaining balance is: $" + (balance - deduction));
